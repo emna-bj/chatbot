7a92362f1690c05d107de5793247b5a600778ff0
emna-bj <emna.beji94@gmail.com>
2021-07-04 21:48:34 +0200

    ajout componsant spellchecker

---
+++ spellchecking.py
@@ -0,0 +1,38 @@
+import typing
+from typing import Any, Optional, Text, Dict, List, Type
+
+from rasa.nlu.components import Component
+from rasa.nlu import utils
+from spellchecker import SpellChecker
+spell = SpellChecker(language='fr')
+if typing.TYPE_CHECKING:
+    from rasa.nlu.model import Metadata
+
+class CorrectSpelling(Component):
+
+    name = "Spell_checker"
+    provides = ["message"]
+    requires = ["message"]
+    language_list = ["fr"]
+
+    def __init__(self, component_config=None):
+        super(CorrectSpelling, self).__init__(component_config)
+
+    def train(self, training_data, cfg, **kwargs):
+        """Not needed, because the the model is pretrained"""
+        pass
+
+
+    def process(self, message, **kwargs):
+        """Retrieve the text message, do spelling correction word by word,
+        then append all the words and form the sentence,
+        pass it to next component of pipeline"""
+        textdata = message.data['text']
+        textdata = textdata.split()
+        new_message = ' '.join(spell.correction(w) for w in textdata)
+        message.data['text'] = new_message
+
+
+    def persist(self,file_name, model_dir):
+        """Pass because a pre-trained model is already persisted"""
+        pass
